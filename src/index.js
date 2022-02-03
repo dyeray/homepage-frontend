cdd656334743bbc3213ea2d934460214719647e7
dyeray <dyeray@gmail.com>
2022-02-03 11:20:47 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FilmList from './FilmList.js'
+
+
+ReactDOM.render(
+    <FilmList />,
+    document.getElementById('root')
+);
