93a27b2b4f852c9d8e3638ff187b0502469090d3
dyeray <dyeray@gmail.com>
2022-02-14 19:14:45 +0000

    Updates

---
+++ precache-manifest.906ad01e3e0285b20686b8a05e260ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b39a85a72484312d953d8ad3220d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d3985081bbe0e11beea",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "49a9dfc3259df4711358",
+    "url": "/static/js/2.95933949.chunk.js"
+  },
+  {
+    "revision": "44f515a80a48c76af8012084c85006fd",
+    "url": "/static/js/2.95933949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3985081bbe0e11beea",
+    "url": "/static/js/main.c6d62ef2.chunk.js"
+  },
+  {
+    "revision": "5636050fdb3f15de4b3c",
+    "url": "/static/js/runtime-main.a7a641b4.js"
+  }
+]);
